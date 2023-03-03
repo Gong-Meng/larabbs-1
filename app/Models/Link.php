913dc320907297eb76632259a3d99f051cf35ca1
Gong Meng <18588264128@163.com>
2023-03-03 20:43:33 +0800

    [扩展包] add spatie/eloquent-sortable

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,8 +2,18 @@
 
 namespace App\Models;
 
-class Link extends Model
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
+
+class Link extends Model implements Sortable
 {
+    use SortableTrait;
+
+    public $sortable = [
+        'order_column_name' => 'order',
+        'sort_when_creating' => true,
+    ];
+
     protected $fillable = ['title', 'link'];
 
     //public $cache_key = 'larabbs_links';
@@ -15,6 +25,7 @@ class Link extends Model
     {
         // 尝试从缓存中取出 cache_key 对应的数据。如果能取到，便直接返回数据。
         // 否则运行匿名函数中的代码来取出活跃用户数据，返回的同时做了缓存。
-        return $this->remember($this->cache_expire_in_minutes)->get();
+        // 尝试从缓存中取出 cache_key 对应的数据。如果能取到，便直接返回数据。
+        return $this->remember($this->cache_expire_in_minutes)->ordered()->get();
     }
 }
